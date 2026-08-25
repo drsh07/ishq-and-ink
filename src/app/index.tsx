14ab639965a3e955ec454f0d1b972aaebeba2a33
Darsh Sood <devdarsood@gmail.com>
2026-08-25 16:45:30 -0700

    added redirect to login screen for testing

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,48 +1,9 @@
-import { Text, View, StyleSheet, TextInput, Button } from "react-native";
-import { useState } from "react";
-import { doc, setDoc, getDoc } from 'firebase/firestore';
-import { db } from "../../firebaseConfig.js"
+import { Redirect } from "expo-router";
 
 export default function Index() {
 
-  const [text, setText] = useState("");
-  const [fetchedText, setFetchedText] = useState("");
-
-  const handleFetch = async () => {
-
-    const docSnap = await getDoc(doc(db, "test", "test1"));
-
-    if (docSnap.exists()) {
-      setFetchedText(docSnap.data().content)
-    }
-    else {
-      setFetchedText("WARNING: Data not found")
-    }
-
-  }
-
   return (
-    <View style={styles.container}>
-      <Text style={{width: "100%"}}>Enter text here:</Text>
-      <TextInput 
-        value={text}
-        placeholder="Type here brooo"
-        onChangeText={setText}
-        style={{width: "100%"}}
-      />
-      <Button title="Submit" onPress={() => {setDoc(doc(db, "test", "test1"), {content: text}); setText("");}} />
-        <View style={{width: "100%"}}>
-      <Button title="Press here to see the text" onPress={() => {handleFetch()}} />
-        </View>
-        <Text>{fetchedText}</Text>
-    </View>
-  );
-}
+    <Redirect href={"/(auth)/login"} />
+  )
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+};
