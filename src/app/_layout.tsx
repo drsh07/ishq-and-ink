4cea32c682500b7172553851ecef3e7dde0fb17c
Darsh Sood <devdarsood@gmail.com>
2026-08-25 16:46:44 -0700

    added fonts and splash screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,32 @@
 import { Stack } from "expo-router";
+import { useFonts, Birthstone_400Regular } from '@expo-google-fonts/birthstone';
+import { Inter_300Light } from '@expo-google-fonts/inter'
+import { Playfair_400Regular } from '@expo-google-fonts/playfair'
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+
+  const [loaded, error] = useFonts({
+    Birthstone_400Regular,
+    Playfair_400Regular,
+    Inter_300Light,
+  })
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error])
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return <Stack>
+    <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+    <Stack.Screen name="(auth)" options={{headerShown: false}} />
+  </Stack>;
 }
