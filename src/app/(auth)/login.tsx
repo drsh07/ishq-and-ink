a356e6aa33a0c301229936ac84b07ced7cbab37e
Darsh Sood <devdarsood@gmail.com>
2026-09-01 01:01:09 -0700

    made theme consistent with write screen and added KeyboardAvoidingView

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -1,8 +1,9 @@
-import { View, StyleSheet, Text, TextInput, Pressable } from "react-native";
+import { View, StyleSheet, Text, TextInput, KeyboardAvoidingView, ScrollView } from "react-native";
 import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth"
 import { auth } from "../../../firebaseConfig"
 import { router, useRouter, Link } from "expo-router";
+import ToolbarButton from "@/components/ToolbarButton";
 
 export default function LoginScreen() {
 
@@ -21,26 +22,37 @@ export default function LoginScreen() {
 
     return (
         <View style={styles.container}>
+        <KeyboardAvoidingView
+        style={{width: "100%", flex: 1, alignItems: "center"}}
+        behavior="padding"
+         >
+            <View style={{flexGrow: 0.4}} />
             <Text style={styles.appHeader}>Ishq & Ink</Text>
-            <TextInput 
-            value={email}
-            placeholder="Email"
-            onChangeText={setEmail}
-            placeholderTextColor={"#797575"}
-            style={styles.textField}
+            <TextInput
+                value={email}
+                placeholder="Email"
+                onChangeText={setEmail}
+                placeholderTextColor={"#797575"}
+                style={styles.textField}
+                autoCapitalize="none"
             />
-            <TextInput 
-            value={passwordText}
-            placeholder="Password"
-            onChangeText={setPasswordText}
-            placeholderTextColor={"#797575"}
-            style={styles.textField}
-            secureTextEntry={true}
+            <TextInput
+                value={passwordText}
+                placeholder="Password"
+                onChangeText={setPasswordText}
+                placeholderTextColor={"#797575"}
+                style={styles.textField}
+                secureTextEntry={true}
+                autoCapitalize="none"
             />
-            <Pressable onPress={handelLogin}>
-                <Text style={styles.button}>Log in</Text>
-            </Pressable>
-            <Link href={"/(auth)/createAccount"}>Create Account</Link>
+            <View style={{
+                marginTop: 50,
+                marginBottom: 10,
+            }}>
+                <ToolbarButton text="Log in" backgroundColor="#ce9ee8" onPress={handelLogin} />
+            </View>
+            <Link href={"/(auth)/createAccount"} style={{ color: "white" }}>Create Account</Link>
+        </KeyboardAvoidingView>
         </View>
     )
 }
@@ -52,22 +64,23 @@ const styles = StyleSheet.create({
         justifyContent: "flex-start",
         alignItems: 'center',
         padding: 25,
+        backgroundColor: "#281b33"
     },
 
     appHeader: {
         fontSize: 80,
         fontFamily: "Birthstone_400Regular",
-        marginTop: 125,
+        color: "white"
     },
 
     textField: {
-        borderColor: "black",
-        borderWidth: 1,
-        width: "100%",
-        borderRadius: 15,
-        padding: 10,
         marginTop: 7,
-        color: "black",
+        backgroundColor: "#443450",
+        borderRadius: 10,
+        padding: 20,
+        color: "white",
+        width: '100%'
+
     },
 
     button: {
