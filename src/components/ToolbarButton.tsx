67c9aca1eace1ad5e8180e9e9a6485b608ab86fd
Darsh Sood <devdarsood@gmail.com>
2026-09-02 02:43:46 -0700

    added visual indication that button is disabled

--- src/components/ToolbarButton.tsx
+++ src/components/ToolbarButton.tsx
@@ -14,7 +14,7 @@ type Props = {
 export default function ToolbarButton({text, backgroundColor = "#281b33", icon, iconColor = "#000000", iconSize = 15, onPress, disabled = false} : Props) {
 
     return (
-        <Pressable style={[styles.container, { backgroundColor } ]} onPress={onPress} android_ripple={{}} disabled={disabled}>
+        <Pressable style={[styles.container, { backgroundColor, opacity: disabled ? 0.4 : 1 } ]} onPress={onPress} android_ripple={{}} disabled={disabled}>
             <Text>{text}</Text>
             {icon && (
                 <MaterialDesignIcons name={icon as any} size={iconSize} color={iconColor} />
