b0f8fbb43b3399e3d005b91e56be98aeb053454c
Darsh Sood <devdarsood@gmail.com>
2026-09-02 03:19:30 -0700

    added loading indicator during authentication check

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Text, TextInput, KeyboardAvoidingView, ScrollView } from "react-native";
+import { View, StyleSheet, Text, TextInput, KeyboardAvoidingView, ScrollView, ActivityIndicator } from "react-native";
 import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth"
 import { auth } from "../../../firebaseConfig"
@@ -9,50 +9,53 @@ export default function LoginScreen() {
 
     const [email, setEmail] = useState("");
     const [passwordText, setPasswordText] = useState("");
+    const [checkingAuth, setCheckingAuth] = useState(false);
 
     const handelLogin = async () => {
         try {
+            setCheckingAuth(true);
             const userCredential = await signInWithEmailAndPassword(auth, email, passwordText);
             const user = userCredential.user;
         }
         catch (error) {
             console.log(error);
+            setCheckingAuth(false);
         }
     }
 
     return (
         <View style={styles.container}>
-        <KeyboardAvoidingView
-        style={{width: "100%", flex: 1, alignItems: "center"}}
-        behavior="padding"
-         >
-            <View style={{flexGrow: 0.4}} />
-            <Text style={styles.appHeader}>Ishq & Ink</Text>
-            <TextInput
-                value={email}
-                placeholder="Email"
-                onChangeText={setEmail}
-                placeholderTextColor={"#797575"}
-                style={styles.textField}
-                autoCapitalize="none"
-            />
-            <TextInput
-                value={passwordText}
-                placeholder="Password"
-                onChangeText={setPasswordText}
-                placeholderTextColor={"#797575"}
-                style={styles.textField}
-                secureTextEntry={true}
-                autoCapitalize="none"
-            />
-            <View style={{
-                marginTop: 50,
-                marginBottom: 10,
-            }}>
-                <ToolbarButton text="Log in" backgroundColor="#ce9ee8" onPress={handelLogin} />
-            </View>
-            <Link href={"/(auth)/createAccount"} style={{ color: "white" }}>Create Account</Link>
-        </KeyboardAvoidingView>
+            <KeyboardAvoidingView
+                style={{ width: "100%", flex: 1, alignItems: "center" }}
+                behavior="padding"
+            >
+                <View style={{ flexGrow: 0.4 }} />
+                <Text style={styles.appHeader}>Ishq & Ink</Text>
+                <TextInput
+                    value={email}
+                    placeholder="Email"
+                    onChangeText={setEmail}
+                    placeholderTextColor={"#797575"}
+                    style={styles.textField}
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    value={passwordText}
+                    placeholder="Password"
+                    onChangeText={setPasswordText}
+                    placeholderTextColor={"#797575"}
+                    style={styles.textField}
+                    secureTextEntry={true}
+                    autoCapitalize="none"
+                />
+                <View style={{
+                    marginTop: 50,
+                    marginBottom: 10,
+                }}>
+                    {checkingAuth ? (<ActivityIndicator size={30} />) : (<ToolbarButton text="Log in" backgroundColor="#ce9ee8" onPress={handelLogin} />)}
+                </View>
+                <Link href={"/(auth)/createAccount"} style={{ color: "white" }}>Create Account</Link>
+            </KeyboardAvoidingView>
         </View>
     )
 }
