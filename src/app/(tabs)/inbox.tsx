0025eeb6cf7060658704f3955b5eb86174b78078
Darsh Sood <devdarsood@gmail.com>
2026-09-02 03:03:01 -0700

    cleaned up inbox screen

--- src/app/(tabs)/inbox.tsx
+++ src/app/(tabs)/inbox.tsx
@@ -1,6 +1,10 @@
-import { View, Text, Pressable, StyleSheet } from "react-native";
+import { View, Text, Pressable, StyleSheet, TextInput, KeyboardAvoidingView, ScrollView, Alert, Keyboard } from "react-native";
 import { signOut } from "firebase/auth";
-import { auth } from "../../../firebaseConfig";
+import { auth, db } from "../../../firebaseConfig";
+import  Header  from '@/components/header';
+import Toolbar from "@/components/toolbar";
+import { useEffect, useState } from 'react';
+import { addDoc, collection, getDoc, doc, serverTimestamp } from "firebase/firestore";
 
 export default function Index() {
 
@@ -15,9 +19,11 @@ export default function Index() {
 
   return (
     <View style={styles.container}>
-      <Pressable onPress={handleSignOut}>
-        <Text style={styles.button}>Log out</Text>
-      </Pressable>
+      <ScrollView style={{}}>
+       <Header profilePress={handleSignOut} />
+     <Text style={{fontFamily: "Playfair_400Regular", color: "white", fontSize: 30}}>Read</Text>
+     <Text style={styles.rules}>Read your partner's letters</Text>
+     </ScrollView>
     </View>
   )
 
@@ -28,16 +34,23 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#281b33",
     flex: 1,
-
+    padding: 25,
   },
 
-  button: {
-        borderColor: "black",
-        borderWidth: 1,
-        borderRadius: 15,
-        padding: 10,
-        backgroundColor: "#ce9ee8",
-        marginTop: 50,
-        marginBottom: 10,
+    rules: {
+      fontFamily: "Inter_300Light",
+      fontSize: 12,
+      color: "white"
+    },
+
+    letterField: {
+      marginTop: 20,
+      backgroundColor: "#443450",
+      borderRadius: 10,
+      flex: 1,
+      padding: 20,
+      textAlignVertical: "top",
+      color: "white",
+
     }
 })
