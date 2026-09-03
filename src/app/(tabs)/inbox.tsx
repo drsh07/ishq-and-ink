629df75064b9547715d0076191e6a6a624961dd8
Darsh Sood <devdarsood@gmail.com>
2026-09-03 00:36:34 -0700

    added list for received letters

--- src/app/(tabs)/inbox.tsx
+++ src/app/(tabs)/inbox.tsx
@@ -1,11 +1,10 @@
-import { View, Text, Pressable, StyleSheet, TextInput, KeyboardAvoidingView, ScrollView, Alert, Keyboard } from "react-native";
+import { View, Text, Pressable, StyleSheet, TextInput, KeyboardAvoidingView, ScrollView, FlatList } from "react-native";
 import { signOut } from "firebase/auth";
 import { auth, db } from "../../../firebaseConfig";
 import Header from '@/components/header';
-import Toolbar from "@/components/toolbar";
+import LetterCard from "@/components/LetterCard";
 import { useEffect, useState } from 'react';
 import { doc, getDocs, collection, query, where, orderBy, onSnapshot, DocumentData } from "firebase/firestore";
-
 
 export default function Index() {
 
@@ -15,7 +14,7 @@ export default function Index() {
 
     const letterQuery = query(collection(db, "letters"), orderBy("timestamp", "desc"), where("from", "==", auth.currentUser!.uid));
     const unsubsribe = onSnapshot(letterQuery, (lettersSnapshot) => {
-      const newLetters = lettersSnapshot.docs.map((letter) => ({id: letter.id, ...letter.data()}));
+      const newLetters = lettersSnapshot.docs.map((letter) => ({ id: letter.id, ...letter.data() }));
       setLetters(newLetters);
     })
     return unsubsribe;
@@ -30,13 +29,22 @@ export default function Index() {
     }
   }
 
+
+
   return (
     <View style={styles.container}>
-      <ScrollView style={{}}>
-        <Header profilePress={handleSignOut} />
-        <Text style={{ fontFamily: "Playfair_400Regular", color: "white", fontSize: 30 }}>Read</Text>
-        <Text style={styles.rules}>Read your partner's letters</Text>
-      </ScrollView>
+      <Header profilePress={handleSignOut} />
+      <Text style={{ fontFamily: "Playfair_400Regular", color: "white", fontSize: 30 }}>Read</Text>
+      <Text style={styles.rules}>Read your partner's letters</Text>
+      <FlatList
+        data={letters}
+        keyExtractor={(letter) => letter.id}
+        renderItem={({item}) => <LetterCard
+         text={item.timestamp ? item.timestamp.toDate().toDateString() + " at " + item.timestamp.toDate().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', hour12: true}) : "Receiving..."}
+         read={item.read}
+         id={item.id}
+         />}
+      />
     </View>
   )
 
@@ -53,17 +61,7 @@ const styles = StyleSheet.create({
   rules: {
     fontFamily: "Inter_300Light",
     fontSize: 12,
-    color: "white"
-  },
-
-  letterField: {
-    marginTop: 20,
-    backgroundColor: "#443450",
-    borderRadius: 10,
-    flex: 1,
-    padding: 20,
-    textAlignVertical: "top",
     color: "white",
-
-  }
+    marginBottom: 20,
+  },
 })
