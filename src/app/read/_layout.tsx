436aabb60fd3ffa448ae16cec18e0e9d69c27c07
Darsh Sood <devdarsood@gmail.com>
2026-09-05 13:29:23 -0700

    ensured text is white across all platforms

--- src/app/read/_layout.tsx
+++ src/app/read/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function LetterLayout() {
-  return <Stack screenOptions={{ headerTintColor: "", title: "Read letter", headerStyle: {backgroundColor: "#443450"} }} />;
+  return <Stack screenOptions={{ headerTintColor: "",
+     title: "Read letter",
+    headerStyle: {backgroundColor: "#443450"},
+    headerTitleStyle: {color: "white"},
+}}
+     />;
 }
