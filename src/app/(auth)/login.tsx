8040af46a9debf89ac745b4139d008d9ab81205b
Darsh Sood <devdarsood@gmail.com>
2026-09-05 23:55:53 -0700

    added login error handling

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -10,6 +10,7 @@ export default function LoginScreen() {
     const [email, setEmail] = useState("");
     const [passwordText, setPasswordText] = useState("");
     const [checkingAuth, setCheckingAuth] = useState(false);
+    const [errorText, setErrorText] = useState("")
 
     const handelLogin = async () => {
         try {
@@ -20,6 +21,25 @@ export default function LoginScreen() {
         catch (error) {
             console.log(error);
             setCheckingAuth(false);
+
+            
+            switch ((error as any).code  ) {
+                case "auth/invalid-email":
+                    setErrorText("Please check your email address");
+                    break;
+                
+                case "auth/wrong-password":
+                case "auth/user-not-found":
+                case "auth/invalid-credential":
+                    setErrorText("Invalid email and/or password");
+                    break;
+                
+                case "auth/user-disabled":
+                    setErrorText("This user has been disabled")
+                
+                case "auth/too-many-requests":
+                    setErrorText("Too many login attempts. Please try again in a few minutes")
+            }
         }
     }
 
@@ -48,11 +68,28 @@ export default function LoginScreen() {
                     secureTextEntry={true}
                     autoCapitalize="none"
                 />
+                <Text style={{
+                    color: "red",
+                    fontFamily: "Inter_300Light",
+                    marginTop: 20,
+                    textAlign: "center"
+                }}>{errorText}</Text>
                 <View style={{
-                    marginTop: 50,
-                    marginBottom: 10,
+                    marginTop: 20,
+                    marginBottom: 30,
                 }}>
-                    {checkingAuth ? (<ActivityIndicator size={30} />) : (<ToolbarButton text="Log in" backgroundColor="#ce9ee8" onPress={handelLogin} />)}
+                    {checkingAuth ? (<>
+                        <ActivityIndicator size={30} />
+                    <Text style={{
+                    color: "white",
+                    fontFamily: "Inter_300Light",
+                    marginTop: 5,
+                    textAlign: "center"
+                }}>Logging in...</Text>
+                </>) :
+                     (
+                     <ToolbarButton text="Log in" backgroundColor="#ce9ee8" onPress={handelLogin} />
+                        )}
                 </View>
                 <Link href={"/(auth)/createAccount"} style={{ color: "white" }}>Create Account</Link>
             </KeyboardAvoidingView>
