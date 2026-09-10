1970de89d268b9ef7084d54862ed3a079ad23ddf
Darsh Sood <devdarsood@gmail.com>
2026-09-10 01:28:31 -0700

    adjusted content to align with actual screens

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -4,7 +4,8 @@ export default function AuthLayout() {
     return (
     <Stack>
     <Stack.Screen name="login" options={{title: "Log in", headerShown: false}}/>
-    <Stack.Screen name="createAccount" options={{title: "Log in", headerShown: false}}/>
+    <Stack.Screen name="createAccount" options={{title: "Create Account", headerStyle: {backgroundColor: "#443450"},
+    headerTitleStyle: {color: "white"}, headerTintColor: "white"}}/>
     </Stack>
     )
 }
