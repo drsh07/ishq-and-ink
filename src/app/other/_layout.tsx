e25e34b07bf9bbcaf6934ae88267053202c6a6a4
Darsh Sood <devdarsood@gmail.com>
2026-09-10 02:01:04 -0700

    ensured header was white

--- src/app/other/_layout.tsx
+++ src/app/other/_layout.tsx
@@ -3,8 +3,8 @@ import { Stack } from "expo-router";
 export default function otherLayout() {
   return (
   <Stack>
-     <Stack.Screen name="editName" options={{ headerTintColor: "editName", title: "Edit Name", headerStyle: {backgroundColor: "#443450"} }} />
-     <Stack.Screen name="about" options={{ headerTintColor: "about", title: "About", headerStyle: {backgroundColor: "#443450"} }} />
+     <Stack.Screen name="editName" options={{ headerTintColor: "white", title: "Edit Name", headerStyle: {backgroundColor: "#443450"} }} />
+     <Stack.Screen name="about" options={{ headerTintColor: "white", title: "About", headerStyle: {backgroundColor: "#443450"} }} />
      </Stack>
   )
 }
